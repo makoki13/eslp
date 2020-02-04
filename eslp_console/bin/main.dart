6870829b2082a617d348cb54135edf3353d61cdc
makoki <pablo.makoki@gmail.com>
2020-02-04 09:20:08 +0100

    dart-console begins

---
+++ eslp_console/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:eslp_console/eslp_console.dart' as eslp_console;
+
+void main(List<String> arguments) {
+  print('Hello world: ${eslp_console.calculate()}!');
+}
---
+++ eslp_console/test/eslp_console_test.dart
@@ -0,0 +1,8 @@
+import 'package:eslp_console/eslp_console.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
