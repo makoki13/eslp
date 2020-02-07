6dca5604aae3869fa2500f1855d5c272f784218c
makoki <pablo.makoki@gmail.com>
2020-02-07 11:31:48 +0100

    arreglo bd provincias

---
+++ eslp_console/bin/tools/test_provincias.dart
@@ -0,0 +1,22 @@
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+
+Future<Object> abreBaseDeDatos() async {
+  var baseDeDatos = 'provincias.db';
+  var dbFactory = databaseFactoryIo;
+
+  // open the database. We use the database factory to open the database
+  var db = await dbFactory.openDatabase(baseDeDatos);
+  return db;
+}
+
+void main() async {   
+  var store = intMapStoreFactory.store();
+ 
+  print('Inicio....');
+  
+  abreBaseDeDatos().then( ( db ) async {      
+    var registro = await store.find(db);                      
+    print('${registro}');
+  });          
+}
